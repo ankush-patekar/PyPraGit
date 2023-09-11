0f936a1b613c549c7d63f07c9d579ebaceaf23e7
Ankush Patekar <ankush.patekar@calsoftinc.com>
2023-09-11 15:29:49 +0530

    file created, wrote and read

--- practice.py
+++ practice.py
@@ -1,1 +1,11 @@
-print("hello")
+import os
+
+with open("test.txt", "w") as f:
+    f.write("Hey there\n")
+    f.write("Hope you are doing well\n")
+    f.write("Thank you!!")
+
+
+with open("test.txt", "r") as f1:
+    data = f1.read()
+    print(data)
