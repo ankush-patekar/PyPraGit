fa63c2540eed9fbd4d90795f3bca716fa5a22a39
Ankush Patekar <ankush.patekar@calsoftinc.com>
2023-09-15 10:01:27 +0530

    constructor info

--- practice.py
+++ practice.py
@@ -1,11 +1,31 @@
-import os
+# import os
+# # Create a file in write mode and add dada in it
+# with open("test.txt", "w") as f:
+#     f.write("Hey there\n")
+#     f.write("Hope you are doing well\n")
+#     f.write("Thank you!!")
+#
+# # Read data from exiting file
+# with open("test.txt", "r") as f1:
+#     data = f1.read()
+#     print(data)
 
-with open("test.txt", "w") as f:
-    f.write("Hey there\n")
-    f.write("Hope you are doing well\n")
-    f.write("Thank you!!")
+# ----------------------------------------------------------
+'''
+Constructor : It is method who calls automatically for any object of class calls.
 
+class Computer():
 
-with open("test.txt", "r") as f1:
-    data = f1.read()
-    print(data)
+    def __init__(self):
+        print("Constructor")
+
+    def config(self):
+        print("RAM:8GB, CPU:4, SSD:512GB")
+#create obj of class
+comp = Computer()
+
+#call the method of class
+comp.config()
+
+'''
+
